1dbd678d0a6fde9c3efb70792a711d87e94fcf1b
Loi Le, Thi Kim <loi.le@enpii.com>
2024-11-29 11:28:53 +0700

    Revert test

--- tests/Unit/Appeara_Alpha/App/WP/Appeara_Alpha_WP_Theme_Test.php
+++ tests/Unit/Appeara_Alpha/App/WP/Appeara_Alpha_WP_Theme_Test.php
@@ -116,84 +116,84 @@ class Appeara_Alpha_WP_Theme_Test extends \PHPUnit\Framework\TestCase {
 		$this->assertTrue( empty( $ressult ), 'nothing return' );
 	}
 
-	// public function test_enqueue_scripts() {
-	// 	$wp_app_mock = Mockery::mock( WP_Application::class );
-	// 	$testing_obj = new Appeara_Alpha_WP_Theme( $wp_app_mock );
-	// 	// We use CodeCeption mock
-	// 	//  to override the function `get_theme_slug`
-	// 	$mock_obj = $this->make(
-	// 		$testing_obj,
-	// 		[
-	// 			'get_theme_slug' => function () {
-	// 				return 'slug';
-	// 			},
-	// 			'get_version' => function () {
-	// 				return '1.0';
-	// 			},
-	// 			'get_base_url' => function () {
-	// 				return 'https://abc.com';
-	// 			},
-	// 		]
-	// 	);
+	public function test_enqueue_scripts() {
+		$wp_app_mock = Mockery::mock( WP_Application::class );
+		$testing_obj = new Appeara_Alpha_WP_Theme( $wp_app_mock );
+		// We use CodeCeption mock
+		//  to override the function `get_theme_slug`
+		$mock_obj = $this->make(
+			$testing_obj,
+			[
+				'get_theme_slug' => function () {
+					return 'slug';
+				},
+				'get_version' => function () {
+					return '1.0';
+				},
+				'get_base_url' => function () {
+					return 'https://abc.com';
+				},
+			]
+		);
 
-	// 	// Mock the global functions
-	// 	// We expect `add_post_type_support()` to be invoked twice with 2 set of arguments
-	// 	$mock_global_functions = $this->createMock( Global_Functions_Mock_Placeholder::class );
+		// Mock the global functions
+		// We expect `add_post_type_support()` to be invoked twice with 2 set of arguments
+		$mock_global_functions = $this->createMock( Global_Functions_Mock_Placeholder::class );
 
-	// 	$mock_global_functions->expects( $this->exactly( 1 ) )
-	// 		->method( 'wp_enqueue_style' )
-	// 		->willReturnMap(
-	// 			[
-	// 				[
-	// 					[ $mock_obj->get_theme_slug() . 'main-style', $mock_obj->get_base_url() . '/public-assets/dist/css/main.css', [], $mock_obj->get_version() ],
-	// 					null,
-	// 				],
-	// 			]
-	// 		);
+		$mock_global_functions->expects( $this->exactly( 1 ) )
+			->method( 'wp_enqueue_style' )
+			->willReturnMap(
+				[
+					[
+						[ $mock_obj->get_theme_slug() . 'main-style', $mock_obj->get_base_url() . '/public-assets/dist/css/main.css', [], $mock_obj->get_version() ],
+						null,
+					],
+				]
+			);
 
-	// 	$mock_global_functions->expects( $this->exactly( 1 ) )
-	// 		->method( 'wp_enqueue_script' )
-	// 		->willReturnMap(
-	// 			[
-	// 				[
-	// 					[ $mock_obj->get_theme_slug() . 'main-script', $mock_obj->get_base_url() . '/public-assets/dist/js/main.js', [], $mock_obj->get_version(), true ],
-	// 					null,
-	// 				],
-	// 			]
-	// 		);
+		$mock_global_functions->expects( $this->exactly( 1 ) )
+			->method( 'wp_enqueue_script' )
+			->willReturnMap(
+				[
+					[
+						[ $mock_obj->get_theme_slug() . 'main-script', $mock_obj->get_base_url() . '/public-assets/dist/js/main.js', [], $mock_obj->get_version(), true ],
+						null,
+					],
+				]
+			);
 
-	// 	$mock_global_functions->expects( $this->any() )
-	// 		->method( 'admin_url' )
-	// 		->willReturnMap(
-	// 			[
-	// 				[
-	// 					[ 'admin-ajax.php' ],
-	// 					'https://abc.com/wp-admin/admin-ajax.php',
-	// 				],
-	// 			]
-	// 		);
+		$mock_global_functions->expects( $this->any() )
+			->method( 'admin_url' )
+			->willReturnMap(
+				[
+					[
+						[ 'admin-ajax.php' ],
+						'https://abc.com/wp-admin/admin-ajax.php',
+					],
+				]
+			);
 
-	// 	$mock_global_functions->expects( $this->any() )
-	// 		->method( 'wp_localize_script' )
-	// 		->willReturnMap(
-	// 			[
-	// 				[
-	// 					[
-	// 						$mock_obj->get_theme_slug() . 'main-localize_script',
-	// 						'wpAjax',
-	// 						[
-	// 							'ajax_url' => 'https://abc.com/wp-admin/admin-ajax.php',
-	// 						],
-	// 					],
-	// 					null,
-	// 				],
-	// 			]
-	// 		);
+		$mock_global_functions->expects( $this->any() )
+			->method( 'wp_localize_script' )
+			->willReturnMap(
+				[
+					[
+						[
+							$mock_obj->get_theme_slug() . 'main-localize_script',
+							'wpAjax',
+							[
+								'ajax_url' => 'https://abc.com/wp-admin/admin-ajax.php',
+							],
+						],
+						null,
+					],
+				]
+			);
 
-	// 	Global_Functions_Mocker::$mock = $mock_global_functions;
+		Global_Functions_Mocker::$mock = $mock_global_functions;
 
-	// 	$ressult = $mock_obj->enqueue_scripts();
+		$ressult = $mock_obj->enqueue_scripts();
 
-	// 	$this->assertTrue( empty( $ressult ), 'nothing return' );
-	// }
+		$this->assertTrue( empty( $ressult ), 'nothing return' );
+	}
 }
